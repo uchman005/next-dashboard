dda2460698e05cb3b91aad539e87d8edcfee78e9
Marcel Obiefuna <obiefunamarcel@gmail.com>
2024-07-13 15:23:32 +0100

    first

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "@/app/ui/global.css"
+import { ReactNode } from "react";
+import { inter } from "@/app/ui/fonts";
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
