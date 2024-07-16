358429c0e233c694f28a8df96876c7d96c906cdc
Marcel Obiefuna <obiefunamarcel@gmail.com>
2024-07-16 09:31:26 +0100

    testing against ppr

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,7 +3,7 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
-export const experimental_ppr = true;
+// export const experimental_ppr = true;
 
 export default function SideNav() {
   return (
